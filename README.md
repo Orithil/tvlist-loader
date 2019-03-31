0a621213b42090a99ee69d56b7e6d9e4987c07db
Herudin Ceurno <herudin.ceurno@gmail.com>
2019-03-31 08:18:18 +0300

    Adds packaging info for setuptools.

---
+++ README.md
@@ -0,0 +1,6 @@
+# TV list uploader
+## WIP
+
+This package provides a way to to process a custom TV list from xlsx file into
+JSON and upload it to waordpress site that uses ACF plugin.
+It uses Splinter/Selenium to fill fields with TV list data.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tvlist-loader-orithil",
+    version="0.1",
+    author="Orithil",
+    author_email="orithil@protonmail.com",
+    description="Script ot upload tvlist on target site.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Orithil/tvlist-loader",
+    packages=setuptools.find_packages(),
+    install_requires=["pandas", "splinter"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
