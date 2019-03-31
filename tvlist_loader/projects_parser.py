c24fbf1cadb14f2343878257fa372c886dbe39ab
Orithil <orithil@protonmail.com>
2019-03-31 11:37:57 +0300

    Adds simple parser to get projects list

---
+++ tvlist_loader/projects_parser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def get_projects(browser, site):
+    browser.visit(site)
+    projects_list = []
+    sitemenu = browser.find_by_css('.block_pop')[1].find_by_css('.themes_row > ul > li > a')
+    for item in sitemenu:
+        projects_list.append(item.html)
+    return projects_list
