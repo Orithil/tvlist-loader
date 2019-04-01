19814655777e32d8eb6006c19d47793185869ff8
Orithil <orithil@protonmail.com>
2019-04-01 07:54:00 +0300

    Converts projects names to upercase

--- tvlist_loader/projects_parser.py
+++ tvlist_loader/projects_parser.py
@@ -5,5 +5,5 @@ def get_projects(browser, site):
     projects_list = []
     sitemenu = browser.find_by_css('.block_pop')[1].find_by_css('.themes_row > ul > li > a')
     for item in sitemenu:
-        projects_list.append(item.html)
+        projects_list.append(item.html.upper())
     return projects_list
