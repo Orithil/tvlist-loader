0f483d35abb0446681d0b024e2a5797622c166d3
Orithil <orithil@protonmail.com>
2019-04-02 10:02:33 +0300

    Adds function to check if entry is project instance

--- tvlist_loader/projects_parser.py
+++ tvlist_loader/projects_parser.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
 def get_projects(browser, site):
+    """ Gets projects list from site and returns it as a list
+    """
     browser.visit(site)
     projects_list = []
     sitemenu = browser.find_by_css('.block_pop')[1].find_by_css('.themes_row > ul > li > a')
     for item in sitemenu:
         projects_list.append(item.html.upper())
     return projects_list
+
+def check_project(name, projects_list):
+    """ Checks if entry in tv list is a project instance.
+        If entry name is identical to project name returns entry name.
+        If some part of entry name matches project name or vice versa
+        returns '1' to mark partial matching.
+        If entry name does not match project name at all returns '2'
+        to mark error.
+    """
+    if name in projects_list:
+        return name
+    else:
+        for project in projects_list:
+            if name in project or project in name:
+                return '1'
+        return '2'
